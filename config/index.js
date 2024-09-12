0bbdaa532466f87a7b3c525177b15fa9f72e89d1
王文银 <wangweny.kok@foxmail.com>
2024-09-12 11:44:19 +0800

    配置github page

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/chicken-music/',
     proxyTable: {
     
     },
@@ -52,7 +52,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/chicken-music/',
 
     /**
      * Source Maps
